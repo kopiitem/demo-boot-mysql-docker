c4e68e3e021f049b6c6c05dec9a765fa95435604
donnylie <lie.donny@gmail.com>
2019-01-04 21:12:53 +0800

    init demo-boot-mysql-docker

---
+++ src/main/java/com/kopiitem/demomysql/DemoMysqlApplication.java
@@ -0,0 +1,62 @@
+package com.kopiitem.demomysql;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.stream.Stream;
+
+
+@SpringBootApplication
+public class DemoMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoMysqlApplication.class, args);
+    }
+
+}
+
+
+@Component
+class CL implements CommandLineRunner {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        Stream.of(new User("Donny"), new User("Wice"), new User("Kensei")).forEach(userRepository::save);
+    }
+}
+
+
+@RepositoryRestResource
+interface UserRepository extends JpaRepository<User, Long> {
+
+}
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+class User {
+    @Id
+    @GeneratedValue
+    Long id;
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
